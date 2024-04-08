dc264cdb75338aa4801750dec20095abc98d7645
Andrew Hsu <andrew311000gmail.com>
2024-04-08 16:03:17 -0700

    add second sprint notes

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,3 +1,12 @@
+// passport-local auth
+// admin functionality (revoke a session)
+// view, edit, delete reminders for the logged in user
+// hide the nav bar if not logged in
+
+// Bonus:
+// 1. When a user creates a new reminder, add an option for a "banner keyword".
+// 2. When a user views a reminder, there should be a banner image related to that reminder on display.
+
 let database = require("../database");
 
 let authController = {
@@ -11,10 +20,12 @@ let authController = {
 
   loginSubmit: (req, res) => {
     // implement later
+    
   },
 
   registerSubmit: (req, res) => {
     // implement later
+    
   },
 };
 
