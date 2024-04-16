dc5bb5b3ef5650963808a692a9d6f61b61c55e87
kratos <sebcro@scari.cc>
2024-04-16 16:17:52 -0700

    Remove throw Error to fix wrong email redirect

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
-let Database = [
+let database = [
   {
+    id: 1,
     name: "cindy",
     email: "cindy123@gmail.com",
     password: "cindy123",
@@ -12,18 +13,32 @@ let Database = [
       },
     ],
   },
+  {
+    id: 2,
+    name: "alex",
+    email: "alex123@gmail.com",
+    password: "alex123",
+    reminders: [
+      {
+        id: 1,
+        title: "Basketball practice",
+        description: "Bring napkins for dribbling",
+        completed: false,
+      },
+    ],
+  },
 ];
 
 const userModel = {
   findOne: (email) => {
-    const user = Database.find((user) => user.email === email);
+    const user = database.find((user) => user.email === email);
     if (user) {
       return user;
     }
-    throw new Error(`Couldn't find user with email: ${email}`);
+    return null;
   },
   findById: (id) => {
-    const user = Database.find((user) => user.id === id);
+    const user = database.find((user) => user.id === id);
     if (user) {
       return user;
     }
@@ -33,5 +48,5 @@ const userModel = {
 
 module.exports = {
   userModel,
-  Database,
+  database,
 };
