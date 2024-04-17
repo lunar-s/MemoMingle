b27a28eef04268d1d27da42606677f2e64ba33b4
kratos <sebcro@scari.cc>
2024-04-16 22:20:06 -0700

    Add createUser

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -21,9 +21,34 @@ function isUserValid(user, password) {
   return user.password === password;
 }
 
-// createUser()
+// createUser(), auth_controller
+const createUser = (res, email, password) => {
+  let userExists = userModel.findOne(email);
+  if (userExists) {
+    res.render("auth/register", {
+      message: "Email taken! Please choose another",
+    });
+    return "Error: Email taken!";
+  }
+  let user = {
+    id: database.length + 1,
+    email: email,
+    password: password,
+    reminders: [
+      {
+        id: 1,
+        title: "Make my first list!",
+        description: "Click create",
+        completed: false,
+      },
+    ],
+  };
+  database.push(user);
+  res.redirect("/login");
+};
 
 module.exports = {
   getUserByEmailIdAndPassword,
   getUserById,
+  createUser,
 };
